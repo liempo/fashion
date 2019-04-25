48567486aecbdc238940755cfa463ae4c9a27712
53n531 <gonzalesalec@gmail.com>
2019-04-25 22:56:06 +0800

    Removed ARCore because I got confused with Sceneform and ARCore

--- app/src/main/java/com/fourcode/clients/fashion/ARActivity.kt
+++ app/src/main/java/com/fourcode/clients/fashion/ARActivity.kt
@@ -2,33 +2,11 @@ package com.fourcode.clients.fashion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.fourcode.clients.fashion.helpers.CameraPermissionHelper
-import org.jetbrains.anko.toast
 
 class ARActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ar)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        if (CameraPermissionHelper.hasCameraPermission(this).not())
-            CameraPermissionHelper.requestCameraPermission(this)
-
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<out String>,
-                                            grantResults: IntArray) {
-
-        if (CameraPermissionHelper.hasCameraPermission(this).not())
-            toast("Camera permission is needed to run this application")
-        if (CameraPermissionHelper.shouldShowRequestPermissionRationale(this))
-            // Permission denied with checking "Do not ask again".
-            CameraPermissionHelper.launchPermissionSettings(this)
-
     }
 }
