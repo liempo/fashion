839b2b4bc81c51a4dda1e630e5d71495f4a3aaac
53n531 <gonzalesalec@gmail.com>
2019-05-03 20:22:17 +0800

    Removed tensorflow

---
+++ app/src/main/java/com/fourcode/clients/fashion/AugmentedImageFragment.kt
@@ -0,0 +1,24 @@
+package com.fourcode.clients.fashion
+
+import com.google.ar.core.AugmentedImageDatabase
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.sceneform.ux.ArFragment
+
+class AugmentedImageFragment: ArFragment() {
+
+    override fun getSessionConfiguration(session: Session?): Config {
+
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+
+        val augmentedImageDb = AugmentedImageDatabase.deserialize(
+            session, context!!.assets.open("database.imgdb"))
+
+        return Config(session).apply {
+            updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
+            augmentedImageDatabase = augmentedImageDb
+        }
+
+    }
+}
