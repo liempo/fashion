6e3d98bc96395f5b7760f815e967f843c92388ce
sith <gonzalesalec@gmail.com>
2019-05-10 06:53:14 +0800

    Added Login and Sign Up (AuthActivity)

---
+++ app/src/main/java/com/fourcode/clients/fashion/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.fourcode.clients.fashion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
